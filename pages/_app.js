5da1f60417e77385370062556fe846ad727f30c2
CRYS <mariano.christopher123@gmail.com>
2022-08-25 20:54:29 +0800

    edit components

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,24 @@
 import "../styles/globals.css"
+import Head from "next/head"
 import { MoralisProvider } from "react-moralis"
 import { NotificationProvider } from "web3uikit"
+import Header from "../components/Header"
 
 function MyApp({ Component, pageProps }) {
     return (
-        <MoralisProvider initializeOnMount={false}>
-            <NotificationProvider>
-                <Component {...pageProps} />
-            </NotificationProvider>
-        </MoralisProvider>
+        <div>
+            <Head>
+                <title>Blockchain Pokemon Day and Night Demo</title>
+                <meta name="description" content="Blockchain Pokemon Demo" />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+            <MoralisProvider initializeOnMount={false}>
+                <NotificationProvider>
+                    <Header />
+                    <Component {...pageProps} />
+                </NotificationProvider>
+            </MoralisProvider>
+        </div>
     )
 }
 
