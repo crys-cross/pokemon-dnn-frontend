53f7d6a17995ee0be7b03160ca5442d32fe69235
CRYS <mariano.christopher123@gmail.com>
2022-08-29 15:45:45 +0800

    edit Mint component

--- components/Mint.jsx
+++ components/Mint.jsx
@@ -1,5 +1,5 @@
 import { useWeb3Contract } from "react-moralis"
-import { abi, contractAddresses } from "../constants"
+import { pokemonNftabi, networkAddresses } from "../constants"
 import { useMoralis } from "react-moralis"
 import { useEffect, useState } from "react"
 import { ethers } from "ethers"
@@ -7,6 +7,53 @@ import { useNotification } from "web3uikit"
 import { Button } from "web3uikit"
 
 const Mint = () => {
+    const { chainId: chainIdHex, isWeb3Enabled } = useMoralis()
+    console.log(parseInt(chainIdHex))
+    const chainId = parseInt(chainIdHex)
+    const lotteryAddress = chainId in networkAddresses ? networkAddresses[chainId][0] : null
+    const [mintFee, setMintFee] = useState("0")
+    const dispatch = useNotification()
+
+    const {
+        runContractFunction: catchNft,
+        isLoading,
+        isFetching,
+    } = useWeb3Contract({
+        abi: pokemonNftabi,
+        contractAddress: lotteryAddress,
+        functionName: "catchNft",
+        params: {},
+        msgValue: mintFee,
+    })
+
+    const { runContractFunction: getMintFee } = useWeb3Contract({
+        abi: pokemonNftabi,
+        contractAddress: lotteryAddress,
+        functionName: "getMintFee",
+        params: {},
+    })
+
+    const updateUI = async () => {
+        const getMintFeeFromCall = (await getMintFee()).toString()
+        setMintFee(getMintFeeFromCall)
+    }
+
+    useEffect(() => {
+        if (isWeb3Enabled) {
+            updateUI()
+        }
+    }, [isWeb3Enabled])
+
+    const handleEnterSuccess = () => {
+        dispatch({
+            type: "sucess",
+            message: "Entered Successfully",
+            title: "Tx Notification",
+            position: "topR",
+            icon: "bell",
+        })
+    }
+
     return (
         <section>
             <div>
