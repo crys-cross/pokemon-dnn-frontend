9526727b2c86498458b9af6999ae91afaf0f0c1f
CRYS <mariano.christopher123@gmail.com>
2022-09-01 22:12:03 +0800

    edit component and added image

--- assets/index.js
+++ assets/index.js
@@ -2,5 +2,7 @@ import facebook from "./facebook.svg"
 import instagram from "./instagram.svg"
 import linkedin from "./linkedin.svg"
 import twitter from "./twitter.svg"
+import day from "./day.png"
+import night from "./night.png"
 
-export { facebook, instagram, linkedin, twitter }
+export { facebook, instagram, linkedin, twitter, day, night }
--- components/Mint.jsx
+++ components/Mint.jsx
@@ -7,6 +7,7 @@ import { ethers } from "ethers"
 import { useNotification } from "web3uikit"
 import { Button } from "web3uikit"
 import Image from "next/image"
+import { day, night } from "../assets/index"
 
 const Mint = () => {
     const { Moralis, isWeb3Enabled, chainId: chainIdHex } = useMoralis()
@@ -14,6 +15,10 @@ const Mint = () => {
     const chainId = parseInt(chainIdHex)
     const pkmnAddress = chainId in networkAddresses ? networkAddresses[chainId][0] : null
     const [mintFee, setMintFee] = useState("0")
+    const [commonCounter, setCommonCounter] = useState("0")
+    const [shinyCounter, setShinyCounter] = useState("0")
+    const [totalMinted, setTotalMinted] = useState("0")
+    const [isItDay, setIsItDay] = useState("false")
     const dispatch = useNotification()
 
     const {
@@ -35,9 +40,40 @@ const Mint = () => {
         params: {},
     })
 
+    const { runContractFunction: getCommonCounter } = useWeb3Contract({
+        abi: pokemonNftabi,
+        contractAddress: pkmnAddress,
+        functionName: "getCommonCounter",
+        params: {},
+    })
+
+    const { runContractFunction: getShinyCounter } = useWeb3Contract({
+        abi: pokemonNftabi,
+        contractAddress: pkmnAddress,
+        functionName: "getShinyCounter",
+        params: {},
+    })
+
+    const { runContractFunction: getTokenCounter } = useWeb3Contract({
+        abi: pokemonNftabi,
+        contractAddress: pkmnAddress,
+        functionName: "getTokenCounter",
+        params: {},
+    })
+
+    const { runContractFunction: getIsItDayTime } = useWeb3Contract({
+        abi: pokemonNftabi,
+        contractAddress: pkmnAddress,
+        functionName: "getIsItDayTime",
+        params: {},
+    })
+
     const updateUI = async () => {
-        const getMintFeeFromCall = (await getMintFee()).toString()
-        setMintFee(getMintFeeFromCall)
+        setMintFee((await getMintFee()).toString())
+        setCommonCounter((await getCommonCounter()).toString())
+        setShinyCounter((await getShinyCounter()).toString())
+        setTotalMinted((await getTokenCounter()).toString())
+        setIsItDay((await getIsItDayTime()).toString())
     }
 
     useEffect(() => {
@@ -62,14 +98,16 @@ const Mint = () => {
         })
     }
 
+    let time = isItDay ? day : night
+
     return (
         <section>
             <div>
-                <Image />
+                <Image src={time} />
                 <p></p>
             </div>
             <div>
-                {lotteryAddress ? (
+                {pkmnAddress ? (
                     <Button
                         onClick={async () =>
                             await catchNft({
@@ -79,7 +117,7 @@ const Mint = () => {
                                 onError: (error) => console.log(error),
                             })
                         }
-                        text="Primary Button"
+                        text={`Catch ${time} PkMn`}
                         theme="outline"
                     />
                 ) : (
